0d797bac93f87850ea02f7a5cec48893305d4518
KingsleyObengjunior <pkjobeng@st.ug.edu.gh>
2024-07-02 21:03:18 +0000

    changed font size to 24

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Paul Kingsley Obeng Junior</Text>
+     <Text style={styles.text}>My name is <Text style={styles.boldText}>Paul Kingsley Obeng Junior</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize: 24,
+  },
+  boldText:{
+    fontWeight: 'bold',
   },
 });
